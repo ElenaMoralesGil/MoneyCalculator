385c6073fe208071c1c70c891005d84af8741fa3
ElenaMoralesGil <elena.morales104@alu.ulpgc.es>
2024-01-19 13:42:13 +0000

    added Mock classes that helps check if the exchange rates calculations work

---
+++ src/main/java/software/ulpgc/moneyCalculator/mocks/MockMain.java
@@ -0,0 +1,19 @@
+package software.ulpgc.moneyCalculator.mocks;
+
+
+
+import software.ulpgc.moneycalculator.*;
+
+import java.util.List;
+
+public class MockMain {
+    public static void main(String[] args) {
+        List<Currency> currencies = new MockCurrencyLoader().load();
+        MoneyDialog moneyDialog = new MockMoneyDialog().define(currencies);
+        CurrencyDialog currencyDialog = new MockCurrencyDialog().define(currencies);
+        MoneyDisplay moneyDisplay = new MockMoneyDisplay();
+        ExchangeRateLoader exchangeRateLoader = new MockExchangeRateLoader();
+        Command command = new ExchangeMoneyCommand(moneyDialog, currencyDialog, exchangeRateLoader, moneyDisplay);
+        command.execute();
+    }
+}
